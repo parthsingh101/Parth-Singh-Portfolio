206b484a6879671f4bf53fa62a62e5fdf589b4fc
parth <parth.singh@adaan.com>
2026-04-02 10:20:13 +0530

    Adjust Hero height to h-210

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,7 +8,7 @@ export function Hero() {
   return (
     <section
       id="home"
-      className=" relative h-180  flex items-center justify-center pt-5 overflow-hidden"
+      className=" relative h-210  flex items-center justify-center pt-5 overflow-hidden"
     >
       {/* 3D Background */}
       <div className="absolute inset-0 z-0">
